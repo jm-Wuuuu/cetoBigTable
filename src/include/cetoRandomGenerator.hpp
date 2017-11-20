a782e223eec29e7eba36edcb5726b108dcc8ecdb
superdogming <gamenwu@gmail.com>
2017-11-20 23:29:44 +0800

    add random generator header file

---
+++ src/include/cetoRandomGenerator.hpp
@@ -0,0 +1,13 @@
+#ifndef CETO_RANDOM_GENERATOR_HPP
+#define CETO_RANDOM_GENERATOR_HPP
+
+namespace ceto
+{
+    class RandomGenerator
+    {
+    public:
+        INT32 getNumber();
+    private:
+    };
+}
+#endif
